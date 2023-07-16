a0e9bb8bccaf9ecfc832b458cb842001a89a61ea
João Victor <106630200+SrJohn369@users.noreply.github.com>
2023-07-16 15:49:27 -0300

    add

--- CriandoAlgunsDados/CandlesTypes.py
+++
@@ -1,3 +0,0 @@
-class CandleType:
-    def func(self):
-        pass
--- TestesDeAprendizadoPython/entendendo_current_candle.py
+++ TestesDeAprendizadoPython/entendendo_current_candle.py
@@ -2,20 +2,21 @@ import MetaTrader5 as mt5
 from datetime import datetime
 import pytz
 import pandas as pd
-from decimal import Decimal
 
 # Conectar ao MetaTrader 5
 if not mt5.initialize():
     print("Falha na inicialização do MetaTrader 5")
     exit(1)
 
-symbol = "EURUSD"  # Obter o símbolo desejado
+symbol = "XAUUSD"  # Obter o símbolo desejado
 timeframe = mt5.TIMEFRAME_M5  # Obter o timeframe desejado (M1, M5, H1, etc.)
-data = datetime(2023, 6, 20, hour=15, minute=30, tzinfo=pytz.timezone("Etc/UTC"))  # Obter a data
+data = datetime(2023, 6, 20, hour=15, minute=20, tzinfo=pytz.timezone("Etc/UTC"))  # Obter a data
 current_candle = mt5.copy_rates_from(symbol, timeframe, data, 1)  # Obter a vela atual
+
 # Exibir informações da vela atual
 pd.set_option('display.max_columns', 500)  # número de colunas
 pd.set_option('display.width', 1500)  # largura máxima da tabela
+
 # Traduzindo e buscando apenas as colunas desejadas
 dataFrame = pd.DataFrame(current_candle)
 dataFrame['time'] = pd.to_datetime(dataFrame['time'], unit='s')
@@ -29,31 +30,54 @@ print(pd.DataFrame(new_df))
 
 ########### CALCULO ############
 # sparar as vars para calculo
-CAL_max = new_df['Máxima']
-CAL_min = new_df['Mínima']
-CAL_Open = new_df['Abertura']
-CAL_Close = new_df['Fechamento']
-# Remover os valores de <class 'pandas.core.series.Series'>
-CAL_max = CAL_max.drop(columns='Máxima')
-CAL_min = CAL_min.drop(columns='Mínima')
-CAL_Open = CAL_Open.drop(columns='Abertura')
-CAL_Close = CAL_Close.drop(columns='Fechamento')
+cal_MAX = new_df['Máxima'].values[0]
+cal_MIN = new_df['Mínima'].values[0]
+cal_OPEN = new_df['Abertura'].values[0]
+cal_CLOSE = new_df['Fechamento'].values[0]
+
 
 # deferença entre maxina e minima
-CAL_diferenca_M_m = CAL_max - CAL_min
-print(CAL_diferenca_M_m)
+def def_MAX_MIN():
+    return cal_MAX - cal_MIN
+
 
 # deferença entre Abertura e Fechamento
-CAL_diferenca_O_C = CAL_Close - CAL_Open
-print(CAL_diferenca_O_C)
+def def_Open_Close():
+    return cal_OPEN - cal_CLOSE
 
-# porcentagem da diferença de Open e Close diante do total Max e Min
-porcent = (CAL_diferenca_O_C * 100) / CAL_diferenca_M_m
-print(porcent)
-# posição da porcentagem na max e min
-half = CAL_diferenca_M_m / 2
-pos_percent = CAL_max - half
-print(pos_percent)
+
+# Identifica se é baixa(0) ou alta(1)
+def identificaVela():
+    if cal_CLOSE > cal_OPEN:
+        return 1
+    elif cal_CLOSE < cal_OPEN:
+        return 0
+
+
+# quantos porcentos tem a deferença entre Abertura e Fechamento da candle
+# a diferença entre Open e Close é a candle inteira
+def porcentVela():
+    return (def_Open_Close() * 100) / def_MAX_MIN()
+
+
+# Dados da candle
+def dadosCandle():
+    # baixa
+    if identificaVela() == 0:
+        def_Max_Open = cal_MAX - cal_OPEN
+        def_Close_Min = cal_CLOSE - cal_MIN
+        return def_Max_Open, def_Close_Min
+    elif identificaVela() == 1:
+        def_Open_Min = cal_OPEN - cal_MIN
+        def_Max_Close = cal_MAX - cal_CLOSE
+        return def_Open_Min, def_Max_Close
+
+
+print(def_MAX_MIN(), "maxima e minima")
+print(def_Open_Close(), "Abertura e fechamento")
+print(identificaVela(), "Baixa" if identificaVela() == 0 else "Alta")
+print(porcentVela(), "Porcentagem")
+print(dadosCandle(), "Dados Candle")
 
 # Desconectar do MetaTrader 5
 mt5.shutdown()
